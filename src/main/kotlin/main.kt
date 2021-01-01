3b49cb9dd999386c55037dfe9460ebe2a7007dd0
sjs <sjs@sonic.net>
2021-01-01 12:51:09 -0800

    add copyright stuff.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,21 @@
+/*
+ *  Copyright 2021 Steven Smith kana-tutor.com
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *
+ *  You may obtain a copy of the License at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ *  either express or implied.
+ *
+ *  See the License for the specific language governing permissions
+ *  and limitations under the License.
+ */
+
 class RpnParserException(message: String) : Exception(message)
 
 @Throws(RpnParserException::class)
@@ -29,7 +47,6 @@ fun rpnCalculate(rpnIn: MutableList<String>, previous: List<Double>? = null): Mu
                 val rVal = rv.removeLast()
                 rv.add(
                     when (token) {
-                        "+" -> rVal + lVal
                         "+" -> rVal + lVal
                         "-" -> rVal - lVal
                         "*" -> rVal * lVal
